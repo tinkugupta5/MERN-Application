314774512adf5c2519cfe21f134d770cf7e9086b
tinkugupta5 <tinku.gupta4@gmail.com>
2023-10-19 19:04:44 +0530

    adding morgan package

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
+
+app.use(morgan('dev')) // this package is used for which route is hitted by user.
+
+
 app.get('/',(req,res,next) => {
     res.send('Working')
 })
+
+app.use((req,res,next) => {
+    next(createHttpErrors.NotFound());
+})
+
+app.use((error,req,res,next) => {
+    error.status = error.status || 500
+    res.status(error.status)
+    res.send(error);
+})
+
 const PORT = process.env.PORT || 3000 
 app.listen(PORT,()=>console.log(`🚀 on port ${PORT}`))
