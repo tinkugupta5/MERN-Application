d83a2fd765b42debf5268d879e4b10822f05cd45
tinkugupta5 <tinku.gupta4@gmail.com>
2023-11-05 01:16:27 +0530

    adding hashing password

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -11,6 +12,26 @@ const UserSchema = new mongoose.Schema({
         required:true,
     },
 })
+
+UserSchema.pre('save', async function (next){
+    try {
+        if(this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password,salt) 
+        this.password = hashedPassword
+        
+        }
+        next()
+    } catch (error) {
+        next(error) 
+    }
+})
+
+
+
+
+
+
 
 const User = mongoose.model('user',UserSchema)
 module.exports=User
