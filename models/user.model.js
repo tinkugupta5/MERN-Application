fbb10a772ff818056af88ee3ada2a488cc6a4ecd
tinkugupta5 <tinku.gupta4@gmail.com>
2023-12-09 22:35:56 +0530

    adidng

--- models/user.model.js
+++ models/user.model.js
@@ -19,7 +19,7 @@ UserSchema.pre('save', async function (next){
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(this.password,salt) 
         this.password = hashedPassword
-        
+         
         }
         next()
     } catch (error) {
